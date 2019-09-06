607a9ab8b03a04fda027d3f265f164f4b9e76462
Yog <36329026+kothiyayogesh@users.noreply.github.com>
2019-09-06 15:14:21 +0530

    Reviewed
    Reviewed and updated comments

--- actions.py
+++ actions.py
@@ -24,13 +24,19 @@ class Zomato:
         '''
         #list storing latitude,longitude...
         location_info=[]
-        print(location)
-        queryString={"query":location}
-        headers = {'Accept': 'application/json', 'user-key': self.api_key}
-        r = requests.get(self.base_url+"locations",params=queryString, headers=headers)
-        data=r.json()
-       # print(data)
 
+        queryString={"query":location}
+
+        headers = {'Accept': 'application/json', 'user-key': self.api_key}
+
+        r = requests.get(self.base_url+"locations",params=queryString, headers=headers)
+
+        data=r.json()
+
+	"""
+		Yogesh: When does the location_suggestions are empty? I see when location is not valid it return empty list. First of all this condition should have been checked during Bing API
+		
+	"""
         if len(data['location_suggestions']) == 0:
             raise Exception('invalid_location')
             
@@ -47,19 +53,21 @@ class Zomato:
         Returns dictionary of all cuisine names and their respective cuisine IDs in a given city.
         """
 
-        headers = {'Accept': 'application/json', 'user-key': self.api_key}
-        queryString={"lat":location_info[0],"lon":location_info[1]}
-        r = (requests.get(self.base_url +"cuisines",params=queryString,headers=headers).content).decode("utf-8")
-        a = ast.literal_eval(r)
-        all_cuisines_in_a_city=a['cuisines']
 
+        headers = {'Accept': 'application/json', 'user-key': self.api_key}
+
+        queryString={"lat":location_info[0],"lon":location_info[1]}
+
+        r = (requests.get(self.base_url +"cuisines",params=queryString,headers=headers).content).decode("utf-8")
+
+        a = ast.literal_eval(r)
+        all_cuisines_in_a_city = a['cuisines']
 
         cuisines={}
 
         for cuisine in all_cuisines_in_a_city:
-            current_cuisine=cuisine['cuisine']
-            cuisines[current_cuisine['cuisine_name'].lower()]=current_cuisine['cuisine_id']
-
+            current_cuisine = cuisine['cuisine']
+            cuisines[current_cuisine['cuisine_name'].lower()] = current_cuisine['cuisine_id']
 
         return cuisines
 
@@ -69,39 +77,42 @@ class Zomato:
         Takes cuisine name and city id as argument.
         Returns the cuisine id for that cuisine.
         '''
-        cusines=self.get_cuisines(location_info)
+        cusines = self.get_cuisines(location_info)
+
         return cusines[cuisine_name.lower()]
 
 
     def get_all_restraunts(self,location,cuisine):
         '''
         Takes city name and cuisine name as arguments.
-        Returns a list of 20 restaurants.
+        Returns a list of 5 restaurants.
         '''
+
         location_info=self.getLocationInfo(location)
         cuisine_id=self.get_cuisine_id(cuisine,location_info)
 
         print(location_info)
         print(cuisine_id)
 
-        queryString={"entity_type":location_info[3],"entity_id":location_info[2],"cuisines":cuisine_id,"count":5}
+        queryString={"entity_type":location_info[3], "entity_id":location_info[2], "cuisines":cuisine_id, "count":5}
 
         headers = {'Accept': 'application/json', 'user-key': self.api_key}
-        r =requests.get(self.base_url + "search",params=queryString, headers=headers)
+        r = requests.get(self.base_url + "search",params=queryString, headers=headers)
 
         list_ofall_rest=r.json()["restaurants"]
+
         names_of_all_rest=[]
         for rest in list_ofall_rest:
             names_of_all_rest.append(rest["restaurant"]["name"])
-
 
         return names_of_all_rest
 
     def get_all_restraunts_without_cuisne(self,location):
         '''
-        Takes city name and cuisine name as arguments.
-        Returns a list of 20 restaurants.
+        Takes city name as arguments.
+        Returns a list of 5 restaurants.
         '''
+
         location_info=self.getLocationInfo(location)
         
         queryString={"entity_type":location_info[3],"entity_id":location_info[2],"count":5}
@@ -128,16 +139,21 @@ class LocationExtractor:
     def getLocationInfo(self,query):
         
         list_cities=[]
+
         queryString={"query":query,"key":self.bing_api_key}
+
         r = requests.get(self.bing_baseurl,params=queryString)
+
         data=r.json()
-        print(data)
+
+	"""
+		Yogesh: Why aren't we checking if coordinate and name key is there in dictionary. If it is not there then it will throw KeyError. Please handle this case.
+	"""
+
+        print("Coordinates are :", data["resourceSets"][0]["resources"][0]["point"]["coordinates"])
+        print("Location is :", data["resourceSets"][0]["resources"][0]["name"])
         return data["resourceSets"][0]["resources"][0]["point"]["coordinates"],data["resourceSets"][0]["resources"][0]["name"]
         
-        #for x in all_locations["annotations"]:
-        #    list_cities.append(x["label"])
-            
-        #return list_cities
 
 class ActionSetLocation(Action):
 
@@ -146,29 +162,38 @@ class ActionSetLocation(Action):
         return "action_set_location"
 
     def run(self, dispatcher,tracker,domain):
+
         user_input=tracker.latest_message['text']
-        le=LocationExtractor()
-        location_name=le.getLocationInfo(str(user_input))
-        print(location_name)
-        
+
+        le = LocationExtractor()
+        location_name = le.getLocationInfo(str(user_input))
+
         return [SlotSet("location",location_name[0])]
 
-
+		
+"""
+	Yogesh: This is redundant function. Functionality is same as ActionShowRestaurants. Remove this and replace all action from story and domain file.
+"""
 class ActionSetCuisine_showRestaurants(Action):
     def name(self):
         return "action_get_cuisine_show_restaurants"
 
 
     def run(self, dispatcher,tracker,domain):
+
         cuisine_type=tracker.get_slot('cuisine')
         location_name=tracker.get_slot('location')
-        print(cuisine_type)
-        print(location_name)
 
-        zom=Zomato()
+        print("Cuisine is: ", cuisine_type)
+        print("Location is: ", location_name)
 
-        list_all_restaurants=zom.get_all_restraunts(str(location_name),str(cuisine_type))
+        zom = Zomato()
+
+        list_all_restaurants = zom.get_all_restraunts(str(location_name),str(cuisine_type))
         
+	"""
+		Yogesh: Rather than sending each restaurant name seperately send a proper response like this: "We found <restaurant 1>, <restaurant 2>, .. of <cuisine_name> cuisine at <location> location. Have a great time :)"
+	"""
         for r in list_all_restaurants:
             dispatcher.utter_message(r)
 
@@ -185,12 +210,13 @@ class GetRestaurantsWithoutCuisine(Action):
 
         location_name=tracker.get_slot('location')
         
-        print(location_name)
+        zom = Zomato()
 
-        zom=Zomato()
-
-        list_all_restaurants=zom.get_all_restraunts_without_cuisne(str(location_name))
+        list_all_restaurants = zom.get_all_restraunts_without_cuisne(str(location_name))
         
+	"""
+		Yogesh: Rather than sending each restaurant name seperately send a proper response like this: "We found <restaurant 1>, <restaurant 2>, .. at <location> location. Have a great time :)"
+	"""
         for r in list_all_restaurants:
             dispatcher.utter_message(r)
 
@@ -205,15 +231,19 @@ class ActionShowRestaurants(Action):
         return "action_show_restaurants"
 
     def run(self, dispatcher,tracker,domain):
-        zo=Zomato()
-        le=LocationExtractor()
-        user_input=tracker.latest_message['text']
-        cuisine_type=tracker.get_slot('cuisine')
-        location_name=le.getLocationInfo(str(user_input))
-        print(user_input)
-        print(cuisine_type)
-        print(location_name)
-        list_all_restaurants=zo.get_all_restraunts(location_name[0],str(cuisine_type))
+
+        zo = Zomato()
+        le = LocationExtractor()
+
+        cuisine_type = tracker.get_slot('cuisine')
+	
+	"""
+		Yogesh: Why are we hitting Bing API again. Best practice is to check if location slot is set or not. If not then hit API. This could occur in scenrario where user says "I would like to eat Italian food in Andheri" 
+	"""
+        user_input = tracker.latest_message['text']
+        location_name = le.getLocationInfo(str(user_input))
+
+        list_all_restaurants = zo.get_all_restraunts(location_name[0],str(cuisine_type))
         
         for r in list_all_restaurants:
             dispatcher.utter_message(r)
